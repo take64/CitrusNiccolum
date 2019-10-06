6254e5a4b9fcba3496662805a2aba45ecc612627
kouhei.takemoto <take64@citrus.tk>
2019-10-06 20:19:37 +0900

    ライブラリ化

---
+++ CitrusNiccolum/CitrusNiccolum.h
@@ -0,0 +1,19 @@
+//
+//  CitrusNiccolum.h
+//  CitrusNiccolum
+//
+//  Created by kouhei.takemoto on 2019/10/06.
+//  Copyright © 2019 citrus.tk. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CitrusNiccolum.
+FOUNDATION_EXPORT double CitrusNiccolumVersionNumber;
+
+//! Project version string for CitrusNiccolum.
+FOUNDATION_EXPORT const unsigned char CitrusNiccolumVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CitrusNiccolum/PublicHeader.h>
+
+
